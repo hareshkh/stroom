412e8a083cfd14f47288527b21702162cb5c0deb
Utsav Mangal <mangalutsav1@gmail.com>
2016-08-13 10:15:11 +0530

    fixed minor syntax error

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express();
 // console.log(app);
-// var expressLayouts = require('express-ejs-layouts');
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
+var expressLayouts = require('express-ejs-layouts');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 app.set('port',(process.env.PORT||6000));
 http.listen (app.get('port'),function(){
   console.log("listening to port number "+app.get('port'));
