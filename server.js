440f56a61a97363ecd78d94b2d4565869a001ac2
Utsav Mangal <mangalutsav1@gmail.com>
2016-08-13 11:15:02 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var app = express();
-// console.log(app);
 var expressLayouts = require('express-ejs-layouts');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -15,4 +14,8 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.get( '/', function(req, res) {
 	res.render('arena');
+});
+app.get( '/create/random', function(req, res)
+{
+	res.render('room');
 });
