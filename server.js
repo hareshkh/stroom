43f86c4408eeaa66a979f1b457ff359885bb83ec
Utsav Mangal <mangalutsav1@gmail.com>
2016-08-14 01:33:12 +0530

    Removes sockets in room name and added random strings

--- server.js
+++ server.js
@@ -11,15 +11,17 @@ app.set('port',(process.env.PORT||3000));
 http.listen (app.get('port'),function() {
   console.log("listening to port number "+app.get('port'));
 });
+var links = ["demoal","specry","credry","phonal","tactly","nating","fracor","medish","logive","duceur","tactly","fratee","medent","logish","cepess","vokous","biblic","verant","malish","pedous","loging","mitish","tenian","regant","scriby","nomman","cyclee","exlegy","venive","geneur","marure","verter","tensly","cluser","bioics","tainic","geoish","tenish","venure","ferity","fracer","rectly","nateur","legous","capess","dictty","pedman","tenent","mining","domian"];
 Room.io = io;
 app.use(express.static(__dirname + '/public'));
 app.use(expressLayouts);
-app.set('layout');
+app.set('layout'); 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.get( '/', function(req, res) {
 	res.render('index');
 });
+
 app.get( '/create/room', function(req, res) {
 	res.render('room');
 });
@@ -31,16 +33,18 @@ var playerInstances = {};
 
 io.on('connection',function(socket){
 	console.log("user connected");
-	var myRoom = "";
+    var x = Math.floor((Math.random() * 50));
+    var y = Math.floor((Math.random() * 50));
+	var myRoom = links[x]+links[y];
 	var room;
-	console.log(socket.handshake.headers.referer);	
-    if (socket.handshake.headers.referer.split('/')[3] == 'create')
+	console.log(socket.handshake.headers.referer);
+
+    if (socket.handshake.headers.referer.split('/')[4] == 'room')
     {
         if(socket.handshake.headers.referer.split('/')[5] == undefined)
             {
-                myRoom = socket.id;
-                room = Room.allocateFirst(socket,socket.id);
-                socket.emit('alertLink',baseUrl+"/room/"+socket.id);
+                room = Room.allocateFirst(socket,myRoom);
+                socket.emit('alertLink',baseUrl+"/room/"+myRoom);
                 console.log("room allocated");  
             }
     }
