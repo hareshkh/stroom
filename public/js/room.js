0fa7ad0f8d194e4d4e9e686a57a1fea4eac38080
Utsav Mangal <mangalutsav1@gmail.com>
2016-08-13 16:31:22 +0530

    modified server

--- public/js/room.js
+++ public/js/room.js
@@ -1,7 +1,5 @@
+console.log("jslinked");
+var socket = io.connect('http://localhost');	
  socket.on('alertLink', function(msg){
     alertLink("Give This Link To Your Friend", msg, true);
-    var clipboard = new Clipboard('button.cancel', {
-        target: function (trigger) {
-            return $(".sweet-alert p:first")[0];
-        }
-    });
+  });
--- server.js
+++ server.js
@@ -8,11 +8,13 @@ app.set('port',(process.env.PORT||3000));
 http.listen (app.get('port'),function() {
   console.log("listening to port number "+app.get('port'));
 });
+Room.io = io;
 app.use(express.static(__dirname + '/public'));
 app.use(expressLayouts);
 app.set('layout');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+console.log(__dirname);
 app.get( '/', function(req, res) {
 	res.render('arena');
 });
@@ -20,12 +22,15 @@ app.get( '/create/room', function(req, res) {
 	res.render('room');
 });
 io.on('connection',function(socket){
+	var str = String(typeof(Room.allocateFirst));
+	alert(str);
 	var myroom = "";
 	var room;
-	if (socket.handshake.headers.referer.split('/')[3] == 'room')
+	console.log(socket.handshake.headers.referer.split('/')[3]);	
+	if (socket.handshake.headers.referer.split('/')[4] == 'room')
 	{
-		console.log("room alloaction staarted");
-		if(games[socket.handshake.headers.referer.split('/')[4]] == undefined)
+		console.log(type);
+		if(socket.handshake.headers.referer.split('/')[5] == undefined)
             {
             	myRoom = socket.id;
             	room = Room.allocateFirst(socket,socket.id);
@@ -34,7 +39,7 @@ io.on('connection',function(socket){
             }
         else
             {
-            	console.log("join my room,Beaches");
+            	console.log("join my room,Beaches");	
                 room = Room.allocateOther(socket,socket.handshake.headers.referer.split('/')[4]);
                 socket.emit("players",[room.playersHandle[0],room.playersHandle[1]]);
             }
